ebf4ddf339ca376605b09dd84e409fb53c950936
zsaritek <zsaritek@gmail.com>
2024-02-02 11:15:33 +0100

    button

--- src/pages/StrategicMeeting.jsx
+++ src/pages/StrategicMeeting.jsx
@@ -17,45 +17,45 @@ const StrategicMeeting = () => {
 
   useEffect(() => {
     const meetingRequest = async () => {
-    if(meeting) {
-        const body = {meeting: selectedDay.toDateString()}
+      if (meeting) {
+        const body = { meeting: selectedDay.toDateString() }
         await companyService.postMeeting(body)
-    } else {
+      } else {
         await companyService.deleteMeeting();
+      }
     }
-  }
 
-  meetingRequest();
-}, [meeting])
+    meetingRequest();
+  }, [meeting])
 
-return (
+  return (
     <div className="container mx-auto p-4 md:p-8">
       <h1 className="text-2xl md:text-3xl font-semibold mb-4 md:mb-6">Strategic Meeting</h1>
-  
+
       <p className="mb-2 md:mb-4">
         Hello valued customer! Our strategic analysis page provides information about the valuable services we offer to guide investment banks in the IPO process for unicorn companies.
       </p>
-  
+
       <p className="mb-2 md:mb-4">
         On our strategic analysis page, you can access detailed information about Clusterwords Analysis, Ratings Analysis, Most Frequent Words Analysis, and other analysis types to gain insights that will help us better understand your business.
       </p>
-  
+
       <p className="mb-2 md:mb-4">
         If you would like to request a detailed meeting or get more information, please arrange your appointment. We are eager to serve you better!
       </p>
-  
+
       {meeting && <h2 className='text-xl md:text-2xl'>Your next strategic meeting: {selectedDay.toDateString()}</h2>}
-  
+
       <div className="mb-4">
         <Calendar onSelect={handleSelect} />
       </div>
-  
-      <button onClick={handleDate} className="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600 w-full md:w-auto">
+
+      <button onClick={handleDate} className="bg-rose-400 text-white py-2 px-4 rounded-full hover:bg-blue-600 w-full md:w-auto">
         {meeting ? <p>Cancel meeting</p> : <p>Request Meeting</p>}
       </button>
     </div>
   );
-  
+
 };
 
 export default StrategicMeeting;
